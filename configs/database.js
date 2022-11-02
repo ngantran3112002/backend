81c02efeb153c04cc8d5de2c8a097846af1c7945
Do Truong <doqq@DESKTOP-I8I8HFV.lan>
2022-11-02 21:02:47 +0700

    new

--- configs/database.js
+++ configs/database.js
@@ -2,11 +2,6 @@ const mysql = require('mysql2/promise')
 const config = require('./config')
 
 
-const query = async (sql, params) => {
-    const connection = await mysql.createConnection(config.db);
-    const [res, ] = await connection.execute(sql, params)
-    return res;
-}
 
 const pool = mysql.createPool(config.db)
 
