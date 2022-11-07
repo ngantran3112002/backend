72119bbcb27b347641cc132474edcdad7a178020
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2022-11-08 00:20:08 +0700

    thêm quan hệ Order - Product

--- controller/orderController.js
+++ controller/orderController.js
@@ -5,46 +5,36 @@ const sequelize = require('../model/Sequelize').sequelize
 const Order = require('../model/orderModel')
 const OrderDetail = require('../model/orderDetailsModel');
 const Payment = require('../model/paymentModel');
+const User = require('../model/userModel')
+const { where } = require('sequelize');
 
 
 
 const insertDetails = 'INSERT INTO admin.order_details (order_id, product_id, modified_at, quantity, created_at) VALUES(?, ?, CURRENT_TIMESTAMP, ?, CURRENT_TIMESTAMP);'
 
 
-const addOrder = asyncHandler(async (req, res) => {
-    const [req_user_id, req_total, req_payment_id, req_orderDetails] = [req.body['user_id'], req.body['total'], req.body['payment_id'], req.body['orderDetails']];
-    await sequelize.transaction(async (transaction) => {
-        const createPayment = await Payment.create({
-            payment_id: true,
-        }).then((results) => {
-            res.json(results);
-        }).catch((err) => {
-            res.json(err);
-        })
+const getOrderInfo = asyncHandler(async (req, res) => {
+    const orderID = req.orderID;
+    let userId = req.userId;
+    // const data  = [{}]
+    const data = await OrderDetail.findAll({
+        where: {
+            order_id: {
+                [Op.eq]: orderID
+            }
+        }
     })
-    // await sequelize.transaction(async (transaction)=> {
-    //     const createOrder = await Order.create({
-    //         user_id: req_user_id,
-    //         total: req_total,
-    //         payment_id: req_payment_id
-    //     },{transaction})
-    //     const orderId = createOrder.get('id');
-    //     const data = req_orderDetails.map(obj => ({'order_id': orderId,...obj}));
-    //     console.log(data);
-    //     await OrderDetail.bulkCreate(data)
-    //     .then(data => {
-    //         res.status(200).json(data);
-    //     }).catch( err => {
-    //         console.log(err);
-    //         res.status(500).json({msg:"error", details : err})
-    //     })
-    // })
-    
+
+    const paymentInfo = await Payment.findAll({
+        where: {
+
+        }
+    })
    
    return res;
 })
 
 
 module.exports = {
-    addOrder
+    getOrderInfo
 }
--- controller/transactionController.js
+++ controller/transactionController.js
@@ -5,41 +5,42 @@ const Order = require('../model/orderModel');
 const OrderDetail = require('../model/orderDetailsModel');
 const Payment = require('../model/paymentModel');
 const Product = require('../model/productModel');
+const User = require('../model/userModel');
+const { or } = require('sequelize');
+
 
 const startTransaction = asyncHandler(async (req, res) => {
     const [req_user_id, req_total, req_orderDetails] = [req.body['user_id'], req.body['total'] ,req.body['orderDetails']];
-    // console.log(req_orderDetails.length   req_orderDetails[i].product_id)
-    for (i = 0; i , i < req_orderDetails.length; i++) {
-        await Product.decrement({quantityInStock: req_orderDetails[i].quantityOrdered},{where: {productId:req_orderDetails[i].product_id }})
-    }
-    
 
-    await sequelize.transaction( (t) => {
-        return  Payment.create({payment_type: true,},{transaction: t})
-        .then((payment)=> { return  Order.create({
-                user_id: req_user_id,
-                payment_id: payment['id'],
-                total: req_total,},{transaction: t})
-                .then((order) => {
-                    // res.json(order)
-                    const orderId = order['id'];
-                    const data = req_orderDetails.map(obj => ({'order_id': orderId,...obj}));
-                    // res.json(data);
-                    console.log(data);
-                    return  OrderDetail.bulkCreate(data, {transaction: t})
-                        .then((orderDetails) => {
-                            return res.json(orderDetails)
-                     })
-                        .catch( (err) => {
-                            console.err(err)
-                        }
-                    )}
-                )
-        }).catch((err) => {
-            res.json(err);
+    const order = await Order.create (
+        {
+            user_id: 1,
+            payment_id: 2,
+            total: 3000,
+            status: true,
+        }
+    )
+    const products = [{productId: 1}, {productId: 5}]
+    for(i = 0; i < products.length; i++) {
+        const product = await Product.findOne({
+            where: {
+                productId: products[i].productId,
+            }
         })
+        await order.addProduct(product, {through: {
+            quantity: 2,
+            priceEach: 1
+        }}).then(() => {
+            console.log("Sucess")
+        })
+    }
+    const results = await Order.findAll({
+        where: {
+            user_id: 1,
+        },
+        include: Product
     })
-   return res.json(req_orderDetails);
+    return res.json(results);
 })
 
 module.exports = {
