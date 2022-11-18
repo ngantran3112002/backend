e244e43e8248c4d0197b75f0d5adf601cbfd758f
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2022-11-18 23:55:01 +0700

    add api for pagination

--- configs/config.js
+++ configs/config.js
@@ -3,7 +3,7 @@ const config = {
         /* don't expose password or any sensitive info, done only for demo */
         host: "localhost",
         user: "root",
-        password: "ASD123qwe@",
+        password: "",
         database: "admin",
         // socketPath: '/var/run/mysqld/mysqld.sock',
         connectionLimit : 100, //important
--- controller/product.controller.js
+++ controller/product.controller.js
@@ -15,6 +15,73 @@ const getAllProducts = async (req, res,next) => {
         }) 
 }
 
+const getProductList = async(req, res, next) => {
+    let page = !isNaN(req.query.page) || !(req.query.page == null)  ? req.query.page : 1;
+    let category =  parseInt(req.query.category);
+    const total = await Product.count();
+    
+    if (category && page) {
+        category = parseInt(category)
+        page = parseInt(page)
+        const totalCate = await Product.count({where: {categoryId : parseInt(category)}})
+        let skip = (page - 1) * 9
+        await Product.findAll({
+            where: {
+                categoryId: category
+            },
+            offset: skip,
+            limit: 9
+        }).then( data => {
+            return res.status(200).json({
+                data,
+                total: totalCate
+            })
+        })
+    } else if (category) {
+        category = parseInt(category)
+        const totalCate = await Product.count({where: {categoryId : parseInt(category)}})
+        await Product.findAll({
+            where: {
+                categoryId: category,
+            }
+        }).then( data => {return res.status(200).json({data, total: totalCate})})
+    } 
+    else if (page) {
+        page = parseInt(page)
+        let skip = (page - 1) * 9
+        Product.findAll({
+            offset: skip,
+            limit: 9
+        }).then( data => {return res.status(200).json({data, total: total})})
+    } else {
+        Product.findAll({
+            // limit: 9
+        }).then(data => {return res.status(200).json({data, total: total})})
+    }
+}
+
+const getProductByCategory = async (req, res,next) => {
+    
+    await Product.count({where: {categoryId: parseInt(req.query.category)}})
+        .then((data) => res.json(data)) 
+        .catch((err) => {
+            const e = new Error("có lỗi khi fetch data từ server")
+            e.stack = e
+            next(e, req, res, next)
+    })
+}
+
+const getTotalProduct = async (req, res,next) => {
+    await Product.count()
+        .then((data) => res.json(data)) 
+        .catch((err) => {
+            const e = new Error("có lỗi khi fetch data từ server")
+            e.stack = e
+            next(e, req, res, next)
+    }) 
+}
+
+
 const getProductDetail = async (req, res, next) => {
     const productID = req.body["productID"];
     const rows = await Product.findOne({
@@ -35,5 +102,6 @@ const getProductDetail = async (req, res, next) => {
 module.exports = {
     getAllProducts,
     getProductDetail,
+    getProductList
 
 }
