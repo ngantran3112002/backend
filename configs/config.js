61e4bfa8ebdb13bc2e281730b846aa9e8bea159f
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2022-11-21 10:33:18 +0700

    config for lab

--- configs/config.js
+++ configs/config.js
@@ -1,10 +1,10 @@
 const config = {
     db: {
         /* don't expose password or any sensitive info, done only for demo */
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "admin",
+        host: "10.102.18.99",
+        user: "w42g9",
+        password: "qwertyuiop",
+        database: "w42g9_web",
         // socketPath: '/var/run/mysqld/mysqld.sock',
         connectionLimit : 100, //important
 
